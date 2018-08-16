cb45b4145f084e00f097d10d93b6ecef23008619
Siskia Erissa <siskiaerissa@gmail.com>
2018-08-16 10:49:58 -0400

    added swagger annotation to Book + BookEndpoint, json contract, and UI

--- bookstore-back/src/main/java/com/pluralsight/bookstore/model/Book.java
+++ bookstore-back/src/main/java/com/pluralsight/bookstore/model/Book.java
@@ -1,167 +1,177 @@
 package com.pluralsight.bookstore.model;
 
-import java.util.Date;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.persistence.*;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
+import java.util.Date;
 
 @Entity
+@ApiModel(description = "Book resource representation")
 public class Book {
-	
-	// ======================================
+
+    // ======================================
     // =             Attributes             =
     // ======================================
-	
-	@Id @GeneratedValue
-	private Long id;
-	
-	@Column(length = 200)
-	@NotNull
-	@Size(min = 1, max = 200)
-	private String title;
-	
-	@Column(length = 10000)
-	@Size(min = 1, max = 10000)
-	private String description;
-	
-	@Column(name = "unit_cost")
-	@Min(1)
-	private Float unitCost;
-	
-	@Column(length = 50)
-	@NotNull
+
+    @Id
+    @GeneratedValue
+    @ApiModelProperty("Identifier")
+    private Long id;
+
+    @Column(length = 200)
+    @NotNull
+    @Size(min = 1, max = 200)
+    @ApiModelProperty("Title of the book")
+    private String title;
+
+    @Column(length = 10000)
+    @Size(min = 1, max = 10000)
+    @ApiModelProperty("Summary describing the book")
+    private String description;
+
+    @Column(name = "unit_cost")
+    @Min(1)
+    @ApiModelProperty("Unit cost")
+    private Float unitCost;
+
+    @Column(length = 50)
+    @NotNull
     @Size(min = 1, max = 50)
-	private String isbn;
-	
-	@Column(name = "publication_date")
-	@Temporal(TemporalType.DATE)
-	@Past
-	private Date publicationDate;
-	
-	@Column(name = "nb_of_pages")
-	private Integer nbOfPages;
-	
-	@Column(name = "image_url")
-	private String imageUrl;
-	
-	@Enumerated
-	private Language language;
-	
-	// ======================================
+    @ApiModelProperty("ISBN number")
+    private String isbn;
+
+    @Column(name = "publication_date")
+    @Temporal(TemporalType.DATE)
+    @Past
+    @ApiModelProperty("Date in which the book has been published")
+    private Date publicationDate;
+
+    @Column(name = "nb_of_pages")
+    @ApiModelProperty("Number of pages")
+    private Integer nbOfPages;
+
+    @Column(name = "image_url")
+    @ApiModelProperty("URL of the image cover")
+    private String imageURL;
+
+    @Enumerated
+    @ApiModelProperty( value = "Language in which the book has been written")
+    private Language language;
+
+    // ======================================
     // =            Constructors            =
     // ======================================
-	
-	public Book() {
-		
-	}
-	
-    public Book(String isbn, String title, Float unitCost, Integer nbOfPages, Language language, Date publicationDate, String imageUrl, String description) {
 
+    public Book() {
+    }
+
+    public Book(String isbn, String title, Float unitCost, Integer nbOfPages, Language language, Date publicationDate, String imageURL, String description) {
         this.isbn = isbn;
         this.title = title;
         this.unitCost = unitCost;
         this.nbOfPages = nbOfPages;
         this.language = language;
         this.publicationDate = publicationDate;
-        this.imageUrl = imageUrl;
+        this.imageURL = imageURL;
         this.description = description;
-
     }
 
     // ======================================
     // =        Getters and Setters         =
     // ======================================
-    
-	public Long getId() {
-		return id;
-	}
 
-	public void setId(Long id) {
-		this.id = id;
-	}
 
-	public String getTitle() {
-		return title;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public void setTitle(String title) {
-		this.title = title;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public String getDescription() {
-		return description;
-	}
+    public String getTitle() {
+        return title;
+    }
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
-	public Float getUnitCost() {
-		return unitCost;
-	}
+    public String getDescription() {
+        return description;
+    }
 
-	public void setUnitCost(Float unitCost) {
-		this.unitCost = unitCost;
-	}
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-	public String getIsbn() {
-		return isbn;
-	}
+    public Float getUnitCost() {
+        return unitCost;
+    }
 
-	public void setIsbn(String isbn) {
-		this.isbn = isbn;
-	}
+    public void setUnitCost(Float unitCost) {
+        this.unitCost = unitCost;
+    }
 
-	public Date getpublicationDate() {
-		return publicationDate;
-	}
+    public String getIsbn() {
+        return isbn;
+    }
 
-	public void setpublicationDate(Date publicationDate) {
-		this.publicationDate = publicationDate;
-	}
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
 
-	public Integer getNbOfPages() {
-		return nbOfPages;
-	}
+    public Date getPublicationDate() {
+        return publicationDate;
+    }
 
-	public void setNbOfPages(Integer nbOfPages) {
-		this.nbOfPages = nbOfPages;
-	}
+    public void setPublicationDate(Date publicationDate) {
+        this.publicationDate = publicationDate;
+    }
 
-	public String getImageUrl() {
-		return imageUrl;
-	}
+    public Language getLanguage() {
+        return language;
+    }
 
-	public void setImageUrl(String imageUrl) {
-		this.imageUrl = imageUrl;
-	}
+    public void setLanguage(Language language) {
+        this.language = language;
+    }
 
-	public Language getLanguage() {
-		return language;
-	}
+    public Integer getNbOfPages() {
+        return nbOfPages;
+    }
 
-	public void setLanguage(Language language) {
-		this.language = language;
-	}
-	
-	// ======================================
+    public void setNbOfPages(Integer nbOfPages) {
+        this.nbOfPages = nbOfPages;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imagURL) {
+        this.imageURL = imagURL;
+    }
+
+    // ======================================
     // =   Methods hash, equals, toString   =
     // ======================================
 
-	@Override
-	public String toString() {
-		 return "Book{" +
-		            "id=" + id +
-		            ", title='" + title + '\'' +
-		            ", description='" + description + '\'' +
-		            ", unitCost=" + unitCost +
-		            ", isbn='" + isbn + '\'' +
-		            ", publicationDate=" + publicationDate +
-		            ", language=" + language +
-		            '}';
-	}
-		
-	
+    @Override
+    public String toString() {
+        return "Book{" +
+            "id=" + id +
+            ", title='" + title + '\'' +
+            ", description='" + description + '\'' +
+            ", unitCost=" + unitCost +
+            ", isbn='" + isbn + '\'' +
+            ", publicationDate=" + publicationDate +
+            ", language=" + language +
+            '}';
+    }
 }
--- bookstore-back/src/main/java/com/pluralsight/bookstore/rest/BookEndpoint.java
+++ bookstore-back/src/main/java/com/pluralsight/bookstore/rest/BookEndpoint.java
@@ -1,28 +1,40 @@
 package com.pluralsight.bookstore.rest;
 
-import java.net.URI;
-import java.util.List;
+import com.pluralsight.bookstore.model.Book;
+import com.pluralsight.bookstore.repository.BookRepository;
+import io.swagger.annotations.*;
 
 import javax.inject.Inject;
 import javax.validation.constraints.Min;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
+import java.net.URI;
+import java.util.List;
 
-import com.pluralsight.bookstore.model.Book;
-import com.pluralsight.bookstore.repository.BookRepository;
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static javax.ws.rs.core.MediaType.TEXT_PLAIN;
 
-import javax.ws.rs.core.MediaType;
-
-
+@SwaggerDefinition(
+    info = @Info(
+        title = "BookStore APIs",
+        description = "BookStore APIs exposed from a Java EE back-end to an Angular front-end",
+        version = "V1.0.0",
+        contact = @Contact(
+            name = "Siskia Erissa",
+            email = "siskiaerissa@gmail.com"
+        )
+    ),
+    host = "localhost:8080",
+    basePath = "/bookstore-back/api",
+    schemes = {SwaggerDefinition.Scheme.HTTP, SwaggerDefinition.Scheme.HTTPS},
+    tags = {
+        @Tag(name = "Book", description = "Tag used to denote operations as private")
+    }
+)
 @Path("/books")
+@Api("Book")
 public class BookEndpoint {
 
     // ======================================
@@ -37,8 +49,13 @@ public class BookEndpoint {
     // ======================================
 
     @POST
-    @Consumes(MediaType.APPLICATION_JSON)
-    public Response createBook(Book book, @Context UriInfo uriInfo) {
+    @Consumes(APPLICATION_JSON)
+    @ApiOperation("Creates a book given a JSon Book representation")
+    @ApiResponses({
+        @ApiResponse(code = 201, message = "The book is created"),
+        @ApiResponse(code = 415, message = "Format is not JSon")
+    })
+    public Response createBook(@ApiParam(value = "Book to be created", required = true) Book book, @Context UriInfo uriInfo) {
         book = bookRepository.create(book);
         URI createdURI = uriInfo.getBaseUriBuilder().path(book.getId().toString()).build();
         return Response.created(createdURI).build();
@@ -46,7 +63,13 @@ public class BookEndpoint {
 
     @GET
     @Path("/{id : \\d+}")
-    @Produces(MediaType.APPLICATION_JSON)
+    @Produces(APPLICATION_JSON)
+    @ApiOperation(value = "Returns a book given an id", response = Book.class)
+    @ApiResponses({
+        @ApiResponse(code = 200, message = "Book found"),
+        @ApiResponse(code = 400, message = "Invalid input. Id cannot be lower than 1"),
+        @ApiResponse(code = 404, message = "Book not found")
+    })
     public Response getBook(@PathParam("id") @Min(1) Long id) {
         Book book = bookRepository.find(id);
 
@@ -58,13 +81,24 @@ public class BookEndpoint {
 
     @DELETE
     @Path("/{id : \\d+}")
+    @ApiOperation("Deletes a book given an id")
+    @ApiResponses({
+        @ApiResponse(code = 204, message = "Book has been deleted"),
+        @ApiResponse(code = 400, message = "Invalid input. Id cannot be lower than 1"),
+        @ApiResponse(code = 500, message = "Book not found")
+    })
     public Response deleteBook(@PathParam("id") @Min(1) Long id) {
         bookRepository.delete(id);
         return Response.noContent().build();
     }
 
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
+    @Produces(APPLICATION_JSON)
+    @ApiOperation(value = "Returns all the books", response = Book.class, responseContainer = "List")
+    @ApiResponses({
+        @ApiResponse(code = 200, message = "Books found"),
+        @ApiResponse(code = 204, message = "No books found"),
+    })
     public Response getBooks() {
         List<Book> books = bookRepository.findAll();
 
@@ -76,7 +110,12 @@ public class BookEndpoint {
 
     @GET
     @Path("/count")
-    @Produces(MediaType.TEXT_PLAIN)
+    @Produces(TEXT_PLAIN)
+    @ApiOperation(value = "Returns the number of books", response = Long.class)
+    @ApiResponses({
+        @ApiResponse(code = 200, message = "Number of books found"),
+        @ApiResponse(code = 204, message = "No books found"),
+    })
     public Response countBooks() {
         Long nbOfBooks = bookRepository.countAll();
 
@@ -85,5 +124,4 @@ public class BookEndpoint {
 
         return Response.ok(nbOfBooks).build();
     }
-    
 }
